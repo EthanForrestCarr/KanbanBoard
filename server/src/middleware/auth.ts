3da006005278463f0f99f9fba7cb0777a2175093
EthanForrestCarr <ethan@ethancarr.com>
2024-11-04 15:14:13 -0600

    fixed return statements and rebuilt

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -10,12 +10,14 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
   const token = authHeader && authHeader.split(' ')[1];
 
   if (!token) {
-    return res.status(401).json({ message: 'Token not provided' });
+    res.status(401).json({ message: 'Token not provided' });
+    return;
   }
 
   jwt.verify(token, process.env.JWT_SECRET_KEY as string, (err, decoded) => {
     if (err) {
-      return res.status(403).json({ message: 'Invalid token' });
+      res.status(403).json({ message: 'Invalid token' });
+      return;
     }
 
     req.user = decoded as JwtPayload;
--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -17,10 +17,10 @@ export const login = async (req: Request, res: Response) => {
       expiresIn: '1h',
     });
 
-    res.json({ token });
+    return res.json({ token });
   } catch (err) {
     console.error('Error during login:', err);
-    res.status(500).json({ message: 'Login failed' });
+    return res.status(500).json({ message: 'Login failed' });
   }
 };
 
